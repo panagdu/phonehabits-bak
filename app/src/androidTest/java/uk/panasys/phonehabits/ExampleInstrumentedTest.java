dc8bf3434792fd644c69b76629830906b0f9dd00
Dumitru Panaghiea <panaghiea@gmail.com>
2017-07-26 09:39:04 +0100

    Initial commit; android app skeleton

---
+++ app/src/androidTest/java/uk/panasys/phonehabits/ExampleInstrumentedTest.java
@@ -0,0 +1,42 @@
+package uk.panasys.phonehabits;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("uk.panasys.phonehabits", appContext.getPackageName());
+    }
+
+    @Test
+    public void testHelloWorld() {
+        onView(withId(R.id.helloWorldText))
+                .check(matches(withText(R.string.hello_world)));
+    }
+}
